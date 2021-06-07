0ff2a65c764ba7bfdfb9fa0451d6fbaf0cc42320
Animesh <54435702+AnimeshRy@users.noreply.github.com>
2021-06-07 15:08:07 +0530

    changed default debug to false

--- portal/settings.py
+++ portal/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv('DEBUG', default=True))
+DEBUG = bool(os.getenv('DEBUG', default=False))
 
 ALLOWED_HOSTS = []
 
